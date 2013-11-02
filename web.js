43f9726a3d707a3a5e82bbfc172f026c134d9b8c
Homero Barrocas Soares Esmeraldo Esmeraldo <homerobse@gmail.com>
2013-11-02 04:08:21 +0000

    testes

--- web.js
+++ web.js
@@ -4,13 +4,13 @@ app.use(express.logger());
 
 var fs = require('fs')
 var buf = fs.readFileSync('index.html');
-var index = buf.toString();
+var html = buf.toString();
 
 app.get('/', function(request, response) {
-  response.send(index);
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
